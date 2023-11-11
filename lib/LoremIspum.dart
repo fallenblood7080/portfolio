90d1e71dc84c00c9a17007e257e4567b5364fcf5
Fallenblood <67326508+fallenblood7080@users.noreply.github.com>
2023-11-11 18:38:34 +0530

    Delete lib directory

--- lib/LoremIspum.dart
+++
@@ -1,6 +0,0 @@
-class LoremIspum{
-  static String GetLoremParagraph(){
-    return "Culpa tempor labore voluptate laboris laboris. Fugiat non nostrud qui ad commodo esse enim pariatur eiusmod voluptate enim nostrud adipisicing anim. Do velit eiusmod cupidatat reprehenderit elit qui id ad.Nostrud consequat enim laboris cillum eiusmod et dolore. Cillum nulla adipisicing non eu in quis in aliquip. Minim consequat non cupidatat consequat eiusmod voluptate cillum et enim. Nulla ea quis irure cupidatat nisi eu dolore dolor. Sit minim voluptate id culpa pariatur ut ex. Enim commodo duis irure enim est.Non excepteur voluptate tempor deserunt culpa anim Lorem veniam mollit. Fugiat quis velit dolor Lorem labore aliquip labore nostrud labore ea ipsum consequat eu. Ut ex veniam quis et non cupidatat do elit sint eiusmod adipisicing enim incididunt sint. Ullamco cillum cupidatat ut minim.";
-  }
-}
-
